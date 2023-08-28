71fa2be508d93c4d60c205609c57781e5809556a
Ddarlin Andres <darlinandresrivas@gmail.com>
2023-08-28 09:10:50 -0500

    crear login y registro de la aplicacion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Login } from "./components/InicioSesion/Login";
+import { Register } from "./components/InicioSesion/Register";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Login />} /> 
+      <Route path="/register" element={<Register />} /> 
       {/* <Route path="*" element={<NotFound />} /> */}
     </Routes>
   );
