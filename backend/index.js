6ba430c114ebac4b11cf25c1970e97522ac92640
Ddarlin Andres <darlinandresrivas@gmail.com>
2023-08-28 12:25:04 -0500

    agregar express session y uuid

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const session = require("express-session");
+const { v4 } = require("uuid");
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
+app.use(
+  session({
+    secret: v4(),
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // Procesamientos:
 app.use(express.json());
