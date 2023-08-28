a96a08c79fe2495307ddc3a758755afd49332b40
Ddarlin Andres <darlinandresrivas@gmail.com>
2023-08-28 09:18:47 -0500

    crear la pagina de inicio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
 import { Login } from "./components/InicioSesion/Login";
 import { Register } from "./components/InicioSesion/Register";
-import { Route, Routes } from "react-router-dom";
+
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} /> 
       <Route path="/register" element={<Register />} /> 
+      <Route path="/home" element={<Home />} /> 
       {/* <Route path="*" element={<NotFound />} /> */}
     </Routes>
   );
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { NavBar } from "../Layout/NavBar"
+import { Footer } from "../Layout/Footer"
+
+export const Home = () => {
+  return (
+    <div>
+        <NavBar />
+        <main>Este es el home</main>
+        <Footer />
+    </div>
+  );
+};
