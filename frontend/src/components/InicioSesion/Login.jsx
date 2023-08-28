77cdea4200579325a8acae4f32bac1ffcd228a8f
19SERGIO <sergiolondono55@gmail.com>
2023-08-28 11:10:59 -0500

    Register y inicio

--- frontend/src/components/InicioSesion/Login.jsx
+++ frontend/src/components/InicioSesion/Login.jsx
@@ -7,31 +7,32 @@ export const Login = () => {
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <a
             href="#"
-            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
             <img className="w-11 h-11 mr-2" src="https://placekitten.com/100/100" alt="logo"/>
-            SE-JustApp
+            <span className="text-transparent bg-clip-text bg-gradient-to-r to-sky-300 from-blue-800">SE-JustApp</span>
+            
           </a>
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-full bg-gradient-to-r from-cyan-200 to-blue-700 border-4  border-x-sky-300  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in to your account
+              <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white italic underline">
+                Inicio de sesion
               </h1>
               <form className="space-y-4 md:space-y-6">
                 <div>
                   <label
-                    for="email"
+                    
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Your email
+                    Correo:
                   </label>
-                  <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                  <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required=""/>
                 </div>
                 <div>
                   <label
-                    for="password"
+                    
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Password
+                    Contraseña:
                   </label>
-                  <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                  <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                 </div>
                 <div className="flex items-center justify-between">
                   <div className="flex items-start">
@@ -40,29 +41,31 @@ export const Login = () => {
                     </div>
                     <div className="ml-3 text-sm">
                       <label
-                        for="remember"
+                        
                         className="text-gray-500 dark:text-gray-300">
-                        Remember me
+                        Recuerdame
                       </label>
                     </div>
                   </div>
                   <a
                     href="#"
                     className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">
-                    Forgot password?
+                    ¿Olvidaste tu contraseña?
                   </a>
                 </div>
                 <button
                   type="submit"
-                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                  Sign in
+                  className=" place-items-center flex flex-col items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-400 to-blue-600 group-hover:from-cyan-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-sky-500 dark:focus:ring-blue-800">
+                  <span className="relative px-11 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                  Inicio de Sesion
+                  </span>
                 </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Don’t have an account yet?{" "}
+                  Tienes cuenta?{" "}
                   <Link
                     to={`/register`}
                     className="font-medium text-primary-600 hover:underline dark:text-primary-500">
-                    Sign up
+                    Registrarse
                   </Link>
                 </p>
               </form>
@@ -70,6 +73,8 @@ export const Login = () => {
           </div>
         </div>
       </section>
+
+      
     </>
   );
 };
