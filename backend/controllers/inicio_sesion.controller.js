6203edd43b46bd9c16bb184e086c498803457bfc
Ddarlin Andres <darlinandresrivas@gmail.com>
2023-09-01 09:18:30 -0500

    agregar estilos al correo de recuperacion

--- backend/controllers/inicio_sesion.controller.js
+++ backend/controllers/inicio_sesion.controller.js
@@ -63,11 +63,111 @@ const recoveryEmail = (req, res) => {
   const email = req.body.email;
 
   const mailOptions = {
-    from: "98c0ccbf2a2e00",
+    from: `${EMAIL}`,
     to: email,
     subject: "Recuperación de Contraseña",
-    html: `<h1 style="color: red;">Recuperacion de Contrase</h1>
-    <strong>${secretKey}</strong>`, // Genera una nueva contraseña
+    html: `<body
+    style="
+      background-color: rgb(255, 255, 255);
+      margin: 0;
+      padding: 0;
+      font-family: Arial, sans-serif;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    "
+  >
+    <article style="width: 80%; height: 100vh; border: 1px solid black">
+      <header
+        style="
+          color: rgb(255, 255, 255);
+          background-color: rgb(0, 119, 255);
+          text-align: center;
+          padding: 10px 0;
+          height: 15%;
+        "
+      >
+        <h1>JustiApp</h1>
+      </header>
+      <main
+        style="
+        width: 100%;
+          background-color: rgb(255, 255, 255);
+          border-color: black;
+          border-width: 1px;
+          height: 70%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
+        "
+      >
+      <br>
+        <main style="width:70%;height: 100%;">
+          <h3
+            style="
+              color: rgb(0, 0, 0);
+              margin-left: 40px;
+              margin: 10px 0;
+              color: #000;
+            "
+          >
+           <b> Recuperacion de Contraseña</b>
+          </h3>
+          <h3
+            style="
+              color: rgb(129, 127, 127);
+              margin-left: 10px;
+              margin-left: 10px;
+            "
+          >
+            para recuperar tu contraseña debes ingresar tu ultima contraseña para hacegurarnos de que eres tu 
+            <span style="color: blue; font-size: 20px">&#x2193;</span>
+          </h3>
+
+          <div>
+            <br>
+            <br>
+            <h4>
+              Contraseña :
+              <br> <br> <br>
+              <strong
+                type="text"
+                name="107Anonimous18"
+                placeholder="107Anonimous18@"
+                style="
+                  padding: 10px;
+                  border: 1px solid black;
+                  border-radius: 5px;
+                "
+              >
+                ${secretKey}</strong
+              >
+            </h4>
+          </div>
+          <h3 style="color: rgb(129, 127, 127); ">
+            
+            <br><br><br><br>  <br><br><br><br>
+            ¿No as podido recuperar tu contrasena?
+            <a href="">Usa Nuestra linea  3136349798  </a>
+          </h3>
+        </main>
+      </main>
+      <footer style="background-color: rgb(0, 119, 255); height: 15%">
+        <h3
+          style="
+            color: rgb(129, 127, 127);
+            color: aliceblue;
+            text-align: center;
+            padding: 40px;
+          "
+        >
+          Copyright © 2023 JustiApp, SENA.
+        </h3>
+      </footer>
+    </article>
+  </body>`, // Genera una nueva contraseña
   };
 
   transport.sendMail(mailOptions, (error) => {
