83c6ea60aa2a17214e3ebd5bf6b6812279fa081c
19SERGIO <sergiolondono55@gmail.com>
2023-09-02 01:42:44 -0500

    Reglamento

--- frontend/src/components/Reglamento/Reglamento.jsx
+++ frontend/src/components/Reglamento/Reglamento.jsx
@@ -1,3 +1,18 @@
-const Reglamento = () => {
-  return <div>Reglamento</div>;
-};
+import { NavBar } from "../Layout/NavBar";
+import { Footer } from "../Layout/Footer";
+
+export const Home = () => {
+
+  return (
+    <div>
+      <NavBar />
+      <main>
+        <div>
+          <img src="" alt="logo" />
+          <h2>Reglamento del aprendiz SENA</h2>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
