b19d0f340015cdbd3bd0544da2580fa505676bfb
brayan <brayan6omez@gmail.com>
2023-09-04 09:39:29 -0500

    modificar ancho del componente footer

--- frontend/src/Layout/Footer.jsx
+++ frontend/src/Layout/Footer.jsx
@@ -94,7 +94,7 @@ export const Footer = () => {
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
-      <div className="mx-auto max-w-6xl py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+      <div className="mx-auto max-w-full py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
           <div className="space-y-8 xl:col-span-1">
             <img
