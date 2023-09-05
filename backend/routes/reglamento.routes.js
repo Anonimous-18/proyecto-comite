d8463f890ece3970c59d6ec7b11addb2a9a73888
Rjchi <richardospina18@gmail.com>
2023-09-04 19:01:21 -0500

    agregar middleware antes de llamar el controlador que trae el reglamento

--- backend/routes/reglamento.routes.js
+++ backend/routes/reglamento.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
 const { getReglamento } = require("../controllers/reglamento.controller.js");
+const { verifyToken } = require("../middlewares/session.meddleware.js");
 
 const router = Router();
 
-router.get("/api/reglamento", getReglamento);
+router.get("/api/reglamento", verifyToken, getReglamento);
 
 module.exports = router;
