2c5c8ef97798ae9959e30872fd10a096c65140c4
Rjchi <richardospina18@gmail.com>
2023-09-05 19:21:31 -0500

    modificar pagina de inicio

--- frontend/src/Layout/NavBar.jsx
+++ frontend/src/Layout/NavBar.jsx
@@ -1,7 +1,6 @@
-import { Fragment, useState, useEffect } from "react";
+import { Fragment, useState } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { NavLink, Link, useNavigate } from "react-router-dom";
-//import logo from"../../img/logo-sena-negro-png-2022"
 
 // ver https://www.npmjs.com/package/react-spinners | https://www.davidhu.io/react-spinners/
 import PulseLoader from "react-spinners/PulseLoader"; // ...
@@ -38,45 +37,6 @@ export const NavBar = () => {
     }
   };
 
-  const solutions = [
-    {
-      name: "Casos",
-      description: "Measure actions your users take",
-      href: "/casos",
-      icon: IconOne,
-    },
-    {
-      name: "Servicios",
-      description: "Create your own targeted content",
-      href: "/servicios",
-      icon: IconTwo,
-    },
-    {
-      name: "Nosotros",
-      description: "Keep track of your growth",
-      href: "/nosotros",
-      icon: IconThree,
-    },
-    {
-      name: "Carreras",
-      description: "Keep track of your growth",
-      href: "/carreras",
-      icon: IconThree,
-    },
-    {
-      name: "Blog",
-      description: "Keep track of your growth",
-      href: "/blog",
-      icon: IconThree,
-    },
-    {
-      name: "Contacto",
-      description: "Keep track of your growth",
-      href: "/contacto",
-      icon: IconThree,
-    },
-  ];
-
   return (
     // Con id queremos identificar cada vez que hacemos scroll
 
@@ -86,7 +46,7 @@ export const NavBar = () => {
     >
       <div className="px-4 sm:px-6">
         {/* Con esto hacemos que el navbar sea responsive ver tailwind.config.js (screen) */}
-        <div className="-ml-4 -mt-2 hidden lg:flex  flex flex-wrap items-center justify-between sm:flex-nowrap md:px-12 px-2">
+        <div className="-ml-4 -mt-2 hidden lg:flex flex-wrap items-center justify-between sm:flex-nowrap md:px-12 px-2">
           <div className="ml-4 mt-2">
             <Link to="/home">
               <img
@@ -138,7 +98,7 @@ export const NavBar = () => {
         </div>
 
         {/* Este es el navLink cuando el de arriba se esconde */}
-        <div className="-ml-4 -mt-2 lg:hidden flex  flex flex-wrap items-center justify-between sm:flex-nowrap md:px-12 px-2">
+        <div className="ml-4 -mt-2 lg:hidden flex flex-wrap items-center justify-between sm:flex-nowrap md:px-12 px-2">
           <div className="ml-4 mt-2">
             <Link to="/home">
               {/* src={logo} */}
@@ -176,30 +136,28 @@ export const NavBar = () => {
                     <Popover.Panel className="absolute -left-20 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
                       <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
                         <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
-                          {solutions.map((item) => (
-                            <NavLink
-                              key={item.name}
-                              to={item.href}
-                              className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
-                            >
-                              <div className="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
-                                <item.icon aria-hidden="true" />
-                              </div>
-                              <div className="ml-4">
-                                <p className="text-sm font-medium text-gray-900">
-                                  {item.name}
-                                </p>
-                                <p className="text-sm text-gray-500">
-                                  {item.description}
-                                </p>
-                              </div>
-                            </NavLink>
-                          ))}
+                          <NavLink
+                            to={`/home`}
+                            className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+                          >
+                            <div className="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
+                              icono
+                            </div>
+                            <div className="ml-4">
+                              <p className="text-sm font-medium text-gray-900">
+                                nombre ejemplo
+                              </p>
+                              <p className="text-sm text-gray-500">
+                                descripcion ejemplo
+                              </p>
+                            </div>
+                          </NavLink>
                         </div>
                         <div className="bg-gray-50 p-4">
                           <button
                             onClick={() => handleClick()}
-                            className="ml-3 relative inline-flex items-center rounded-md border border-transparent bg-black px-10 py-2 text-lg font-bold text-white shadow-xl transition duration-300 ease-in-out hover:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-offset-2"
+                            // Transicion transition duration-300 ease-in-out ...
+                            className="ml-3 relative inline-flex items-center rounded-md border border-transparent bg-black px-10 py-2 text-lg font-bold text-white hover:text-black shadow-xl transition duration-300 ease-in-out hover:bg-white focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-offset-2"
                           >
                             Cerrar sesion
                             <PulseLoader
@@ -222,82 +180,3 @@ export const NavBar = () => {
     </nav>
   );
 };
-
-function IconOne() {
-  return (
-    <svg
-      width="48"
-      height="48"
-      viewBox="0 0 48 48"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect width="48" height="48" rx="8" fill="#FFEDD5" />
-      <path
-        d="M24 11L35.2583 17.5V30.5L24 37L12.7417 30.5V17.5L24 11Z"
-        stroke="#FB923C"
-        strokeWidth="2"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M16.7417 19.8094V28.1906L24 32.3812L31.2584 28.1906V19.8094L24 15.6188L16.7417 19.8094Z"
-        stroke="#FDBA74"
-        strokeWidth="2"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M20.7417 22.1196V25.882L24 27.7632L27.2584 25.882V22.1196L24 20.2384L20.7417 22.1196Z"
-        stroke="#FDBA74"
-        strokeWidth="2"
-      />
-    </svg>
-  );
-}
-
-function IconTwo() {
-  return (
-    <svg
-      width="48"
-      height="48"
-      viewBox="0 0 48 48"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect width="48" height="48" rx="8" fill="#FFEDD5" />
-      <path
-        d="M28.0413 20L23.9998 13L19.9585 20M32.0828 27.0001L36.1242 34H28.0415M19.9585 34H11.8755L15.9171 27"
-        stroke="#FB923C"
-        strokeWidth="2"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M18.804 30H29.1963L24.0001 21L18.804 30Z"
-        stroke="#FDBA74"
-        strokeWidth="2"
-      />
-    </svg>
-  );
-}
-
-function IconThree() {
-  return (
-    <svg
-      width="48"
-      height="48"
-      viewBox="0 0 48 48"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect width="48" height="48" rx="8" fill="#FFEDD5" />
-      <rect x="13" y="32" width="2" height="4" fill="#FDBA74" />
-      <rect x="17" y="28" width="2" height="8" fill="#FDBA74" />
-      <rect x="21" y="24" width="2" height="12" fill="#FDBA74" />
-      <rect x="25" y="20" width="2" height="16" fill="#FDBA74" />
-      <rect x="29" y="16" width="2" height="20" fill="#FB923C" />
-      <rect x="33" y="12" width="2" height="24" fill="#FB923C" />
-    </svg>
-  );
-}
