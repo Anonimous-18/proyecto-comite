369215a2de349cfebe1c1a172d4eecda59c225b5
19SERGIO <sergiolondono55@gmail.com>
2023-09-06 10:01:52 -0500

    Iconos invitados

--- frontend/src/pages/aprendiz/Pruebas_Aprendiz.jsx
+++ frontend/src/pages/aprendiz/Pruebas_Aprendiz.jsx
@@ -12,11 +12,11 @@ export const Pruebas_Aprendiz = () => {
                         
                         <form action="#" className="border-2 border-blue-800 border-dashed p-5 place-content-evenly rounded-2xl" >
                             <div className="sm:col-span-2">
-                                <label for="description" className="block mb-2 text-sm font-medium text-gray-900 ">Descripcion</label>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 ">Descripcion</label>
                                 <textarea id="description" rows="8" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder="Ingrese su descripcion aqui..."></textarea>
                             </div>
                             <div className="w-full">
-                                <label for="brand" className="block mb-2 text-sm font-medium text-gray-900 ">Adjuntar evidencias</label>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 ">Adjuntar evidencias</label>
                                 <input type="file" name="brand" id="brand" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " />
 
                             </div>
--- frontend/src/pages/invitado/Homeinvitado.jsx
+++ frontend/src/pages/invitado/Homeinvitado.jsx
@@ -1,85 +1,73 @@
 import { NavBar } from "../../Layout/NavBar";
 import { Footer } from "../../Layout/Footer";
 import { Link } from "react-router-dom";
+import { GrBook } from "react-icons/gr";
+import {GrDocumentUser} from "react-icons/gr"
 
-
-
-export const Homeinvitado= () => {
-    
-    return (
-        <div>
-            <NavBar /> 
-            <div className="mx-auto max-w-lg pt-20 pb-32 sm:pt-48 sm:pb-40 ">
-                <div className="border-2 shadow-2xl p-5 place-content-evenly rounded-2xl" >
-                    <Link to={"/novedadinvitado"}>
-                    <button className=" hover:bg-blue-200 rounded-xl border-blue-500 shadow-xl ">
-                        <figure className="md:flex border-gray-200 border-2 rounded-xl p-3 md:p-0 max-w-md" >
-                        <img src={"../../img/logo-sena-negro-png-2022.png"} alt="" className=" w-36 h-24 md:h-auto  rounded-xl mx-auto border-2  bg-gray-200" width="350" height="350"/>
-                        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-                            <blockquote>
-                            <div className="text-blue-800 text-lg font-bold">
-                                Ingresar Novedad
-                            </div>
-                            </blockquote>
-                            <blockquote>
-                                <p className="text-sm">
-                                    Podrás ingresar novedades sobre diferentes aprendices, teniendo en cuenta el reglamento del aprendiz.
-                                </p>
-                            </blockquote>
-                            <blockquote>
-                            <p className="text-xs text-gray-600">
-                                    SENA
-                                </p>
-                                <p className="text-xs text-gray-600">
-                                    Regional Caladas
-                                </p>
-                                <p className="text-xs text-gray-600">
-                                    Centro de Automatizacion
-                                </p>
-                            </blockquote>
-                        </div>
-                        </figure>   
-                    </button>
-                    </Link>
-                    
-                    <br /><br />
-                    <Link to={"/reglamento"}>
-                    <button className="hover:bg-blue-200 rounded-xl border-blue-500 shadow-xl">
-                        <figure className="md:flex border-gray-200 border-2 rounded-xl p-3 md:p-0 max-w-md" >
-                        <img src={"../../img/logo-sena-negro-png-2022.png"} alt="" className=" w-36 h-24 md:h-auto  rounded-xl mx-auto border-2  bg-gray-200" width="350" height="350"/>
-                        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-                            <blockquote>
-                            <div className="text-blue-800 text-lg font-bold">
-                                Reglamento del Aprendiz
-                            </div>
-                            </blockquote>
-                            <blockquote>
-                                <p className="text-sm">
-                                    Son los derechos y las obligaciones que tienen las personas matriculadas en alguno de los cursos del SENA.   .
-                                </p>
-                            </blockquote>
-                            <blockquote>
-                                <p className="text-xs text-gray-600">
-                                    SENA
-                                </p>
-                                <p className="text-xs text-gray-600">
-                                    Regional Caladas
-                                </p>
-                                <p className="text-xs text-gray-600">
-                                    Centro de Automatizacion
-                                </p>
-                            </blockquote>
-                        </div>
-                        </figure>   
-                    </button>
-                    </Link>
-                    
-                    
+export const Homeinvitado = () => {
+  return (
+    <div>
+      <NavBar />
+      <div className="mx-auto max-w-4xl  pt-20 pb-32 sm:pt-48 sm:pb-40 ">
+        <div className="h-auto max-w-full flex flex-col items-center border-2 shadow-2xl p-5 place-content-evenly rounded-2xl">
+          <Link to={"/novedadinvitado"} className="p-4">
+            <button className=" hover:bg-blue-200 rounded-xl border-blue-500 shadow-xl ">
+              <figure className="md:flex border-gray-200 border-2 rounded-xl p-3 md:p-0 max-w-2xl">
+                <GrDocumentUser className=" w-64 h-36 md:h-auto  rounded-xl mx-auto border-2  bg-gray-200 p-2"/>
+                <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+                  <blockquote>
+                    <div className="text-blue-800 text-lg font-bold">
+                      Ingresar Novedad
+                    </div>
+                  </blockquote>
+                  <blockquote>
+                    <p className="text-sm">
+                      Podrás ingresar novedades sobre diferentes aprendices,
+                      teniendo en cuenta el reglamento del aprendiz.
+                    </p>
+                  </blockquote>
+                  <blockquote>
+                    <p className="text-xs text-gray-600">SENA</p>
+                    <p className="text-xs text-gray-600">Regional Caladas</p>
+                    <p className="text-xs text-gray-600">
+                      Centro de Automatizacion
+                    </p>
+                  </blockquote>
                 </div>
-            </div>
-            <Footer /> 
-        </div>
-        
-    );
-};
+              </figure>
+            </button>
+          </Link>
 
+          <Link to={"/reglamento"}>
+            <button className="hover:bg-blue-200 rounded-xl border-blue-500 shadow-xl">
+              <figure className="md:flex border-gray-200 border-2 rounded-xl p-3 md:p-0 max-w-2xl">
+                <GrBook className=" w-64 h-36 md:h-auto  rounded-xl mx-auto border-2  bg-gray-200 p-2" height="350"/>
+                <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+                  <blockquote>
+                    <div className="text-blue-800 text-lg font-bold">
+                      Reglamento del Aprendiz
+                    </div>
+                  </blockquote>
+                  <blockquote>
+                    <p className="text-sm">
+                      Son los derechos y las obligaciones que tienen las
+                      personas matriculadas en alguno de los cursos del SENA. .
+                    </p>
+                  </blockquote>
+                  <blockquote>
+                    <p className="text-xs text-gray-600">SENA</p>
+                    <p className="text-xs text-gray-600">Regional Caladas</p>
+                    <p className="text-xs text-gray-600">
+                      Centro de Automatizacion
+                    </p>
+                  </blockquote>
+                </div>
+              </figure>
+            </button>
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
