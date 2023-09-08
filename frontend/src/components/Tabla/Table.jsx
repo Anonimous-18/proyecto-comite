c47716db426ce628210ab37972af5286e0915df5
Rjchi <richardospina18@gmail.com>
2023-09-08 17:25:18 -0500

    modificar el componente table

--- frontend/src/components/Tabla/Table.jsx
+++ frontend/src/components/Tabla/Table.jsx
@@ -1,49 +1,51 @@
 import { NavBar } from "../../Layout/NavBar";
 import { Footer } from "../../Layout/Footer";
-import { useNavigate } from "react-router-dom";
-import { useContextApp } from "../../Context/ContextApp";
 import { useEffect, useState } from "react";
 
-export const Table = () => {
-  const [roles, setRoles] = useState();
-  const { getRoles, filterRol, protectedRoutes, validateToken } =
-    useContextApp();
-  const navigate = useNavigate();
-  const tokenExist = protectedRoutes();
-
+export const Table = ({ datos }) => {
+  const [data, setData] = useState([]);
   useEffect(() => {
-    if (!tokenExist) {
-      navigate(`/`);
-    } else if (validateToken()) {
-      navigate(`/`);
-    } else {
-      const token = JSON.parse(localStorage.getItem("newToken"));
-      const admin = localStorage.getItem("admin");
-      if (admin) {
-        const getRol = async () => {
-          const res = await getRoles(token.token);
-          if (res !== null || res !== undefined) {
-            setRoles(res);
-          }
-        };
-
-        getRol();
-      }
+    if (datos) {
+      setData(datos);
     }
-  }, [navigate, tokenExist, filterRol, getRoles, validateToken]);
+  }, [datos]);
 
+  if (datos.length === 0) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
       <NavBar />
       <div className="h-full w-full mt-28">
-        {/* <ul>
-
-        {
-            roles.map((rol.id) => (
-                <li key={id}></li>
-                ))
-            }
-            </ul> */}
+        {data.map((dato, index) => {
+          const columnas = Object.keys(dato);
+          return (
+            <table key={index} className="bg-rose-500 border border-black">
+              <thead>
+                <tr>
+                  {columnas.map((columna) => (
+                    <th key={columna}>{columna.toUpperCase()}</th>
+                  ))}
+                  <th>ACCIONES</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  {columnas.map((columna) => (
+                    <td className="px-11" key={columna}>
+                      {dato[columna]}
+                    </td>
+                  ))}
+                  <td>
+                    <button className="bg-cyan-600 p-2" type="button">
+                      Ver
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          );
+        })}
       </div>
       <Footer />
     </>
