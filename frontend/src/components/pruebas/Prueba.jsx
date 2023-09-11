f5697da4a292bc749274d9e6dbaf9668c962f939
Ddarlin Andres <darlinandresrivas@gmail.com>
2023-09-11 11:19:04 -0500

    excel a json

--- frontend/src/components/pruebas/Prueba.jsx
+++ frontend/src/components/pruebas/Prueba.jsx
@@ -5,18 +5,7 @@ import { handleFileUpload } from "../../execelJson/xlsxJson";
 
 const Prueba = () => {
   const [excelData, setExcelData] = useState([]); // Estado para almacenar los datos del archivo Excel
-<<<<<<< HEAD
-  console.log(excelData)
-  const onFileUpload = async (event) => {
-    const data = await handleFileUpload(event); // Llama a la función handleFileUpload
-    setExcelData(data);
-    console.log(excelData)
-  };
-  console.log(excelData)
-=======
->>>>>>> 7adfa1f4271ed595f9992cc8267eb4f89c2722b8
 
-  
   const onFileUpload = (event) => {
     const data = handleFileUpload(event);
     console.log(data);
--- frontend/src/execelJson/xlsxJson.js
+++ frontend/src/execelJson/xlsxJson.js
@@ -1,11 +1,6 @@
 import * as XLSX from 'xlsx';
 
 export const handleFileUpload = (event) => {
-<<<<<<< HEAD
-  console.log(event);
-  let excelData = null;
-=======
->>>>>>> 7adfa1f4271ed595f9992cc8267eb4f89c2722b8
   const file = event.target.files[0];
   const excelData = []; 
   if (file) {
@@ -18,10 +13,6 @@ export const handleFileUpload = (event) => {
       const firstSheet = workbook.Sheets[workbook.SheetNames[0]];
       excelData.push(XLSX.utils.sheet_to_json(firstSheet));
     };
-<<<<<<< HEAD
-    
-=======
->>>>>>> 7adfa1f4271ed595f9992cc8267eb4f89c2722b8
     reader.readAsArrayBuffer(file);
   }
   console.log(excelData);
