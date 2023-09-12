7a0fd69319632ab2a48b99a813b5198b8722250d
brayan <brayan6omez@gmail.com>
2023-09-12 11:53:14 -0500

    se creo un boton para renderizar el componente

--- frontend/src/components/pruebas/Prueba.jsx
+++ frontend/src/components/pruebas/Prueba.jsx
@@ -1,27 +1,31 @@
-// import XLSX from 'xlsx';
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { handleFileUpload } from "../../execelJson/xlsxJson";
-// import axios from "axios";
 
 const Prueba = () => {
-  const camposIniciales = {
-    nombres: "",
-    apellidos: "",
-    documento: "",
-  };
-  const [persona, setPersona] = useState(camposIniciales);
-  const [excelData, setExcelData] = useState([]); // Estado para almacenar los datos del archivo Excel
+  const [excelData, setExcelData] = useState([]); // Estado para almacenar los datos del archivo Excel // Estado para almacenar los datos del archivo Excel
+  const [forceRenderKey, setForceRenderKey] = useState(0);
+
   const onFileUpload = (event) => {
     const data = handleFileUpload(event);
-    console.log(data);
     setExcelData(data);
-    
-  }
+  };
+
+  useEffect(() => {
+    // Esta función se ejecutará después de la actualización del estado.
+    if (excelData.length > 0) {
+      console.log("La longitud de excelData es:", excelData.length);
+    }
+  }, [excelData]);
+
+  const forceRender = () => {
+    // Incrementa la clave para forzar la renderización
+    setForceRenderKey((prevKey) => prevKey + 1);
+  };
   return (
     <div>
-      <input type="file" accept=".xlsx" onChange={(e) =>onFileUpload(e)} />
-      <pre>{JSON.stringify(excelData , null, 2)}</pre>
-
+      <input type="file" accept=".xlsx" onChange={(e) => onFileUpload(e)} />
+      <pre>{JSON.stringify(excelData[0], null, 2)}</pre>
+      <button onClick={forceRender}>Forzar Renderización</button>
     </div>
   );
 };
