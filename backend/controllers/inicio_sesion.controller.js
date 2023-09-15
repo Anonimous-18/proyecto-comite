cd77a009ca43195f454cc22a6b1c678422df176c
BrayanGN <brayan6omez@gmail.com>
2023-09-14 19:17:50 -0500

    manejo de errores

--- backend/controllers/inicio_sesion.controller.js
+++ backend/controllers/inicio_sesion.controller.js
@@ -256,15 +256,6 @@ const registerUser = async (req, res) => {
  * |  Controlador del registro de usuarios 
  ------------------------------------------*/
 const registerUsers = async (req, res) => {
-  
-  sequelize.sync()
-  .then(() => {
-    console.log('');
-  })
-  .catch((error) => {
-    console.error('Error al conectar a la base de datos:', error);
-  });
-
   const buscarRolAprendiz = async () => {
     try {
       if(req.body.cargo !== "Aprendiz"){
@@ -279,37 +270,50 @@ const registerUsers = async (req, res) => {
   };
 
   try {
+    // Conecta a la base de datos
+    await sequelize.sync();
     const rolAprendiz = await buscarRolAprendiz();
-    const {data} = req.body;
-    if (data) {
-      data.map((usuario,index) => {
-        usuarios
-          .create({
-            nombre_completo: usuario["Nombre Completo"],
-            telefono: usuario.Telefono,
-            tipo_documento: usuario["Tipo documento"],
-            email: usuario.Email,
-            cargo: usuario.Cargo,
-            dependencia: usuario.Dependencia,
-            documento: usuario.Documento,
-            contrasenia: v4().substring(0, 8),
-            rol_id: rolAprendiz
-          })
-          .then((usuarioCreado) => {
-            console.log("Registro creado con éxito:", usuarioCreado.toJSON());
-          })
-          .catch((error) => {
-            console.error("Error al crear el registro:", error);
-          });
-      });
+    // Función asincrónica para crear usuarios y manejar errores
+    const crearUsuarioConErrorHandling = async (usuario) => {
+      try {
+        const usuarioCreado = await usuarios.create({
+          nombre_completo: usuario["Nombre Completo"],
+          telefono: usuario.Telefono,
+          tipo_documento: usuario["Tipo documento"],
+          email: usuario.Email,
+          cargo: usuario.Cargo,
+          dependencia: usuario.Dependencia,
+          documento: usuario.Documento,
+          contrasenia: v4().substring(0, 8),
+          rol_id: rolAprendiz,
+        });
+
+        return { usuarioCreado, error: null };
+      } catch (error) {
+        return { usuarioCreado: null, error };
+      }
+    };
+
+    const { data } = req.body;
+
+    if (data && Array.isArray(data)) {
+      // Crear un arreglo de promesas usando async/await
+      const promesasInsercion = data.map(crearUsuarioConErrorHandling);
+
+      // Esperar a que todas las inserciones se completen
+      const resultadosInsercion = await Promise.all(promesasInsercion);
+
+      // Envía una respuesta con información sobre los resultados de la inserción
+      res.status(200).json({ resultadosInsercion });
     } else {
       return res
-        .status(500)
-        .json({ message: "Error al intentar registrarse." });
+        .status(400)
+        .json({ message: "Datos inválidos o faltantes en la solicitud." });
     }
   } catch (error) {
+    console.error('Error al conectar a la base de datos:', error);
     res.status(500).json({
-      message: `Error al intentar registrarse detalles: ${error.message}`,
+      message: `Error al intentar registrarse: ${error.message}`,
     });
   }
 };
