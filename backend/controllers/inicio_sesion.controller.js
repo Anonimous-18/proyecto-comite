e89f45a1bf0c248b6142fdf2ecc2d17d16f2f67e
brayan <brayan6omez@gmail.com>
2023-09-18 11:41:14 -0500

    enviar correo a usuarios

--- backend/controllers/inicio_sesion.controller.js
+++ backend/controllers/inicio_sesion.controller.js
@@ -268,6 +268,159 @@ const registerUsers = async (req, res) => {
       throw error;
     }
   };
+  const enviarUsuario = (resultadosInsercion) => {
+    try {
+      resultadosInsercion.map(usuarioEstado=>{
+        if (usuarioEstado.usuarioCreado) {
+          console.log(usuarioEstado.usuarioCreado.dataValues);
+          const email = usuarioEstado.usuarioCreado.dataValues.email;
+          const documento = usuarioEstado.usuarioCreado.dataValues.documento;
+          const contrasenia = usuarioEstado.usuarioCreado.dataValues.contrasenia;
+          const mailOptions = {
+            from: `${EMAIL}`,
+            to: email,
+            subject: "Recuperación de Contraseña",
+            html: `<body
+                style="
+                  background-color: rgb(255, 255, 255);
+                  margin: 0;
+                  padding: 0;
+                  font-family: Arial, sans-serif;
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                  flex-direction: column;
+                "
+              >
+                <article style="width: 80%; height: 100vh; border: 1px solid black">
+                  <header
+                    style="
+                      color: rgb(255, 255, 255);
+                      background-color: rgb(0, 119, 255);
+                      text-align: center;
+                      padding: 10px 0;
+                      height: 15%;
+                    "
+                  >
+                    <h1>JustiApp</h1>
+                  </header>
+                  <main
+                    style="
+                    width: 100%;
+                      background-color: rgb(255, 255, 255);
+                      border-color: black;
+                      border-width: 1px;
+                      height: 70%;
+                      display: flex;
+                      justify-content: center;
+                      align-items: center;
+                      flex-direction: column;
+                    "
+                  >
+                  <br>
+                    <main style="width:70%;height: 100%;">
+                      <h3
+                        style="
+                          color: rgb(0, 0, 0);
+                          margin-left: 40px;
+                          margin: 10px 0;
+                          color: #000;
+                        "
+                      >
+                       <b> Usuario</b>
+                      </h3>
+                      <h3
+                        style="
+                          color: rgb(129, 127, 127);
+                          margin-left: 10px;
+                          margin-left: 10px;
+                        "
+                      >
+                        usuario 
+                        <span style="color: blue; font-size: 20px">&#x2193;</span>
+                      </h3>
+            
+                      <div>
+                        <br>
+                        <br>
+                        <h4>
+                          Contraseña :
+                          <br> <br> <br>
+                          <strong
+                            type="text"
+                            name="107Anonimous18"
+                            placeholder="107Anonimous18@"
+                            style="
+                              padding: 10px;
+                              border: 1px solid black;
+                              border-radius: 5px;
+                            "
+                          >
+                            ${documento}</strong
+                          >
+                          <br> <br> <br>
+                          <strong
+                            type="text"
+                            name="107Anonimous18"
+                            placeholder="107Anonimous18@"
+                            style="
+                              padding: 10px;
+                              border: 1px solid black;
+                              border-radius: 5px;
+                            "
+                          >
+                            ${contrasenia}</strong
+                          >
+                        </h4>
+                      </div>
+                      <h3 style="color: rgb(129, 127, 127); ">
+                        
+                        <br><br><br><br>  <br><br><br><br>
+                        ¿No as podido recuperar tu contrasena?
+                        <a href="">Usa Nuestra linea  3136349798  </a>
+                      </h3>
+                    </main>
+                  </main>
+                  <footer style="background-color: rgb(0, 119, 255); height: 15%">
+                    <h3
+                      style="
+                        color: rgb(129, 127, 127);
+                        color: aliceblue;
+                        text-align: center;
+                        padding: 40px;
+                      "
+                    >
+                      Copyright © 2023 JustiApp, SENA.
+                    </h3>
+                  </footer>
+                </article>
+              </body>`,
+          };
+
+          transport.sendMail(mailOptions, (error) => {
+            if (error) {
+              return res
+                .status(500)
+                .send(`Error al enviar el correo de recuperación a ${email}.`);
+            } else {
+              return res
+                .status(200)
+                .json({ message: "Correo enviado correctamente" });
+            }
+          });
+
+          console.log('prueba')
+        } else {
+          console.log(usuarioEstado.usuarioCreado);
+        }
+
+      }) 
+    } catch (error) {
+      return res.status(500).json({
+        message: `Error al recuperar contraseña detalles ${error.message}`,
+      });
+    }
+  };
 
   try {
     // Conecta a la base de datos
@@ -301,9 +454,8 @@ const registerUsers = async (req, res) => {
       const promesasInsercion = data.map(crearUsuarioConErrorHandling);
 
       // Esperar a que todas las inserciones se completen
-      const resultadosInsercion = await Promise.all(promesasInsercion);
-
-      // Envía una respuesta con información sobre los resultados de la inserción
+      const resultadosInsercion = await Promise.all(promesasInsercion);// Envía una respuesta con información sobre los resultados de la inserción
+      enviarUsuario(resultadosInsercion);
       res.status(200).json({ resultadosInsercion });
     } else {
       return res
