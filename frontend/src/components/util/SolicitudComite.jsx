32e163380d54d609b0421b9852349c0f37928bec
Rjchi <richardospina18@gmail.com>
2023-09-18 16:19:38 -0500

    agregar funcionalidad al crear una solicitud de comite

--- frontend/src/components/util/SolicitudComite.jsx
+++ frontend/src/components/util/SolicitudComite.jsx
@@ -2,19 +2,30 @@ import { useEffect, useState } from "react";
 import { Footer } from "../../Layout/Footer";
 import { NavBar } from "../../Layout/NavBar";
 import { useContextApp } from "../../Context/ContextApp";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const SolicitudComite = () => {
   const [reglamento, setReglamento] = useState([]);
+  const [articulosSeleccionados, setArticulosSeleccionados] = useState([]);
+
   const [data, setData] = useState({
-    capitulo: 1,
+    capitulo: "1",
+    articulo: "",
+    tipo_falta: "",
+    descripcion_falta: "",
+    anexos: "",
+    identificaciones: [],
   });
+
   const { getReglamento, orderReglamento, protectedRoutes, validateToken } =
     useContextApp();
   const navigate = useNavigate();
   const tokenExist = protectedRoutes();
 
   useEffect(() => {
+    /**-------------------------------------------------------
+     * |  Validamos el token de sesión
+     -------------------------------------------------------*/
     if (!tokenExist) {
       navigate(`/`);
     } else if (validateToken()) {
@@ -33,11 +44,39 @@ export const SolicitudComite = () => {
     }
   }, [getReglamento, validateToken, navigate, tokenExist, data.capitulo]);
 
+  const agregarArticulo = () => {
+    /**-------------------------------------------------------
+     * |  Agregamos un nuevo articulo al estado
+     -------------------------------------------------------*/
+    if (data.articulo !== "") {
+      setArticulosSeleccionados([...articulosSeleccionados, data.articulo]);
+      // setData({ ...data, articulo: "" });
+    }
+  };
+
+  const quitarArticulo = (index) => {
+    /**-------------------------------------------------------
+     * |  Eliminamos un articulo splice(desde, hasta)
+     -------------------------------------------------------*/
+    const nuevosArticulos = [...articulosSeleccionados];
+    nuevosArticulos.splice(index, 1);
+    setArticulosSeleccionados(nuevosArticulos);
+  };
+
   const onChange = (e) => {
     setData({
       ...data,
       [e.target.name]: e.target.value,
     });
+  };
+
+  const getArticulos = (reglamentoCompleto) => {
+    /**-------------------------------------------------------------
+     * |  Obtenemos los articulos para un capitulo en especifico(id)
+     -------------------------------------------------------------*/
+    return reglamentoCompleto.find(
+      (item) => item.cap_id === parseInt(data.capitulo)
+    );
   };
 
   if (reglamento === null || reglamento === undefined) {
@@ -47,67 +86,197 @@ export const SolicitudComite = () => {
   }
   const result = orderReglamento(reglamento);
 
+  const agregarIdentificacion = () => {
+    setData({
+      ...data,
+      /**-------------------------------------------------------
+     * |  Agrega una nueva identificación vacia
+     -------------------------------------------------------*/
+      identificaciones: [...data.identificaciones, ""],
+    });
+  };
+
+  const actualizarIdentificacion = (index, value) => {
+    const nuevasIdentificaciones = [...data.identificaciones];
+    nuevasIdentificaciones[index] = value;
+
+    setData({
+      ...data,
+      identificaciones: nuevasIdentificaciones,
+    });
+  };
+
+  const getCapitulos = (elemento) => {
+    /**----------------------------------------------------------
+     * |  Obtenemos el capitulo al que pertenece un articulo id
+     ----------------------------------------------------------*/
+    return result.find(
+      (objeto) =>
+        objeto.contenido &&
+        objeto.contenido.some((i) => i.art_id === parseInt(elemento))
+    );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /**-------------------------------------------------------
+     * |  Eliminamos los elementos repetidos en el array
+     -------------------------------------------------------*/
+    const artRequest = [...new Set(articulosSeleccionados)];
+    let capitulos = [];
+    artRequest.map((e) => capitulos.push(getCapitulos(e).cap_id));
+
+    const capRequest = [...new Set(capitulos)];
+    const identificaciones = [...new Set(data.identificaciones)];
+
+    /**-----------------------------------
+     * |  Eliminamos los valores vacios
+     -----------------------------------*/
+    const idenRequest = identificaciones.filter((valor) => valor !== "");
+
+    console.log("ARTICULOS ", artRequest);
+    console.log("CAPITULOS ", capRequest);
+    console.log("IDENTIFICACIONES ", idenRequest);
+    console.log("DESCRIPCIONFALTA ", data.descripcion_falta);
+    console.log("TIPOFALTA ", data.tipo_falta);
+    console.log("ANEXOS ", data.anexos);
+  };
+
   return (
     <main className="mt-24 h-full w-full">
       <NavBar />
       <div className="h-full w-full">
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <h1>Crear Solicitud Comite</h1>
           <div>
-            <label className="">Capitulo</label>
+            <label className="">Capitulo del Reglamento</label>
             <select
               onChange={(e) => onChange(e)}
               id="capitulo"
               name="capitulo"
               value={data.capitulo}
               required
-              className="bg-rose-200">
-              {result.map((capitulo, index) => (
-                <option key={index} value={`${capitulo.cap_id}`}>
+              className="bg-rose-200"
+            >
+              {result.map((capitulo) => (
+                <option key={capitulo.cap_id} value={`${capitulo.cap_id}`}>
                   Capitulo {capitulo.cap_id} {capitulo.cap_titulo}
                 </option>
               ))}
             </select>
           </div>
           <div>
-            <label className="">Articulo</label>
-            <select
-              // onChange={(e) => onChange(e)}
-              id="articulo"
-              name="articulo"
-              // value={data.tipo_documento}
-              required
-              className="bg-rose-200">
-              <option value="articulo 1">Articulo</option>;
-            </select>
+            <label className="">Articulo del Reglamento</label>
+            <div>
+              <select
+                onChange={(e) => onChange(e)}
+                id="articulo"
+                name="articulo"
+                value={data.articulo}
+                className="bg-rose-200"
+              >
+                <option value="">Seleccione un Articulo</option>
+                {getArticulos(result).contenido.map((articulo, index) => (
+                  <option key={index} value={`${articulo.art_id}`}>
+                    Articulo {articulo.art_id} {articulo.art_titulo}
+                  </option>
+                ))}
+              </select>
+              <button
+                type="button"
+                className="bg-cyan-800"
+                onClick={agregarArticulo}
+              >
+                Agregar Articulo
+              </button>
+            </div>
           </div>
+          {articulosSeleccionados.length > 0 && (
+            <div>
+              <h4>Articulos Seleccionados:</h4>
+              <ul>
+                {articulosSeleccionados.map((articuloId, index) => (
+                  <li key={index}>
+                    {`Articulo ${articuloId}`}
+                    <button
+                      type="button"
+                      className="bg-purple-900"
+                      onClick={() => quitarArticulo(index)}
+                    >
+                      Eliminar
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
           <div>
             <h4>Aprendices Implicados:</h4>
-            <input type="text" name="" className="border border-black" />
+            {data.identificaciones.map((identificacion, index) => (
+              <div key={index}>
+                <input
+                  type="text"
+                  value={identificacion}
+                  onChange={(e) =>
+                    actualizarIdentificacion(index, e.target.value)
+                  }
+                  className="border border-black"
+                  placeholder="Identificacion"
+                />
+              </div>
+            ))}
+            <button
+              type="button"
+              className="bg-yellow-400"
+              onClick={agregarIdentificacion}
+            >
+              {data.identificaciones.length !== 0 ? (
+                <>Agregar Otra Identificación</>
+              ) : (
+                <>Agregar Identificación</>
+              )}
+            </button>
           </div>
           <div>
             <p>Descripcion de la Falta:</p>
-            <textarea name="" className="border border-black" />
+            <textarea
+              name="descripcion_falta"
+              required
+              onChange={(e) => onChange(e)}
+              className="border border-black"
+            />
           </div>
           <div>
             <label className="">Tipo de Falta</label>
             <select
-              // onChange={(e) => onChange(e)}
+              onChange={(e) => onChange(e)}
               id="tipo_falta"
               name="tipo_falta"
-              // value={data.tipo_documento}
+              value={data.tipo_falta}
               required
-              className="bg-rose-200">
-              <option value="academica">Academica</option>
+              className="bg-rose-200"
+            >
+              <option value="">Seleccione el Tipo de Falta</option>
+              <option value="Academica">Academica</option>
+              <option value="Disciplinaria">Disciplinaria</option>
             </select>
           </div>
           <div>
             <p>Carpeta Anexos:</p>
-            <input type="text" name="" className="border border-black" />
+            <input
+              type="text"
+              name="anexos"
+              placeholder="Link de la carpeta"
+              onChange={(e) => onChange(e)}
+              className="border border-black"
+            />
           </div>
-          <div className="bg-rose-500 inline-block cursor-pointer">
+          <Link
+            to={`/home`}
+            className="bg-rose-500 inline-block cursor-pointer"
+          >
             Cancelar
-          </div>
+          </Link>
           <button className="bg-blue-500">Crear Solicitud</button>
         </form>
       </div>
