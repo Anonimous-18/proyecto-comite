9223dff0beb73b769974bd6af723c1b46338c07a
19SERGIO <sergiolondono55@gmail.com>
2023-09-18 23:00:32 -0500

    tabla admin modificando

--- frontend/src/components/Tabla/Table.jsx
+++ frontend/src/components/Tabla/Table.jsx
@@ -43,9 +43,10 @@ export const Table = ({ datos, fun_eliminar, nombre_tabla }) => {
         <div className="h-auto max-w-full flex flex-col items-center  p-5 place-content-evenly rounded-2xl">
           <div className="h-full w-full mt-28">
             <div className="p-2">
+            
               <Link
                 to={`/form-${nombre_tabla}`}
-                className="bg-cyan-600 p-2"
+                className="place-items-center flex flex-col items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-600 to-blue-800 group-hover:from-blue-600 group-hover:to-blue-800 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-sky-500 dark:focus:ring-blue-800"
                 type="button"
               >
                 CREAR
@@ -54,23 +55,23 @@ export const Table = ({ datos, fun_eliminar, nombre_tabla }) => {
             {data.map((dato, index) => {
               const columnas = Object.keys(dato);
               return (
-                <table key={index} className="bg-rose-500 border border-black max-w-full flex flex-col">
+                <table key={index} className="bg-blue-300 border-2 border-black max-w-full flex flex-col">
                   <thead>
                     <tr className="h-auto max-w-full flex flex-row items-center ">
                       {columnas.map((columna) => (
-                        <th className="px-5" key={columna}>{columna.toUpperCase()}</th>
+                        <th className="px-5 " key={columna}>{columna.toUpperCase()}</th>
                       ))}
                       
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
+                    <tr className=" flex flex-row tems-center">
                       {columnas.map((columna) => (
                         <td className=" px-5" key={columna}>
                           {dato[columna]}
                         </td>
                       ))}
-                      <td className="flex flex-row-reverse">
+                      <td className="flex flex-row-reverse items-end ml-10">
                         <Link
                           to={`/see-${nombre_tabla}/${dato.id}`}
                           className=" p-2"
