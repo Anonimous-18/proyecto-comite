fac2aa258968a58af7464364bc041d7fc83a5e52
BrayanGN <brayan6omez@gmail.com>
2023-09-19 21:00:01 -0500

    adjuntar excel responsive

--- frontend/src/components/pruebas/Prueba.jsx
+++ frontend/src/components/pruebas/Prueba.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { handleFileUpload } from "../../execelJson/xlsxJson";
+import { AiFillFileExcel } from "react-icons/ai"
 import axios from 'axios'; 
 const Prueba = () => {
   const [excelData, setExcelData] = useState([]); // Estado para almacenar los datos del archivo Excel // Estado para almacenar los datos del archivo Excel
@@ -32,9 +33,9 @@ const Prueba = () => {
       
       <label
         htmlFor="archivo"
-        className="cursor-pointer flex items-center justify-center w-full md:w-64 h-12 md:h-16 px-4 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 focus:bg-blue-600 text-2xl"
+        className="cursor-pointer flex items-center justify-center w-2/7 p-9 m-6 text-4xl sm:w-2/4 sm:text-9xl sm:h-96 h-68 px-4 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 focus:bg-blue-600 "
       >
-        <span>Seleccionar archivo</span>
+        <span><AiFillFileExcel/></span>
       </label>
       {/* Elemento de entrada oculto para seleccionar archivos */}
       <input
@@ -43,10 +44,10 @@ const Prueba = () => {
         name="archivo"
         accept=".xlsx"
         className="hidden"
-        onChange={(e) => onFileUpload(e)}
+        onChange={(e) => {onFileUpload(e)}}
       />
       <pre>{JSON.stringify(excelData[0], null, 2)}</pre>
-      <button onClick={()=>{ forceRender();enviarUsuarios(); }}>Enviar</button>
+      <button className="cursor-pointer flex items-center justify-center w-55 m-6 sm:w-64 h-12 sm:h-16 px-4 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 focus:bg-blue-600 text-2xl" onClick={()=>{ forceRender();enviarUsuarios(); }} >Enviar</button>
     </div>
     
   );
