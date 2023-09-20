55f39f3dc23bcf1598f0712a57aca68eeb83f8e3
BrayanGN <brayan6omez@gmail.com>
2023-09-19 22:47:53 -0500

    se le agrego un opacidad para que se note el event

--- frontend/src/components/pruebas/Prueba.jsx
+++ frontend/src/components/pruebas/Prueba.jsx
@@ -1,16 +1,21 @@
 import { useState, useEffect } from "react";
-import { handleFileUpload } from "../../execelJson/xlsxJson";
-import { AiFillFileExcel } from "react-icons/ai"
+import { handleFileUpload,excelNombre } from "../../execelJson/xlsxJson";
+import { BsFiletypeExe } from "react-icons/bs"
 import axios from 'axios'; 
 const Prueba = () => {
   const [excelData, setExcelData] = useState([]); // Estado para almacenar los datos del archivo Excel // Estado para almacenar los datos del archivo Excel
+  const [archivoExistente, setArchivoExistente] = useState(null); // Estado para almacenar los datos del archivo Excel // Estado para almacenar los datos del archivo Excel
   const [forceRenderKey, setForceRenderKey] = useState(0);
 
   const onFileUpload = (event) => {
     const data = handleFileUpload(event);
     setExcelData(data);
+    const nombreValido = excelNombre(event)
+    if (nombreValido) {
+      setArchivoExistente(nombreValido);
+    }
   };
-
+  
   const enviarUsuarios = async()=>{
     await axios.post('http://localhost:5000/api/register/users', {
       data: excelData[0],
@@ -33,9 +38,16 @@ const Prueba = () => {
       
       <label
         htmlFor="archivo"
-        className="cursor-pointer flex items-center justify-center w-2/7 p-9 m-6 text-4xl sm:w-2/4 sm:text-9xl sm:h-96 h-68 px-4 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 focus:bg-blue-600 "
+        className={`cursor-pointer flex items-center justify-center w-2/7 p-9 m-6 text-4xl sm:w-2/4 sm:text-9xl sm:h-96 h-68 px-4 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 focus:bg-blue-600 ${archivoExistente?'opacity-50':''}`}
       >
-        <span><AiFillFileExcel/></span>
+        {
+          !archivoExistente?(
+            <span><BsFiletypeExe/></span>
+          ):(
+            <span className="text-4xl" >{archivoExistente}</span>
+          )
+        }
+        
       </label>
       {/* Elemento de entrada oculto para seleccionar archivos */}
       <input
@@ -44,11 +56,11 @@ const Prueba = () => {
         name="archivo"
         accept=".xlsx"
         className="hidden"
-        onChange={(e) => {onFileUpload(e)}}
+        onChange={(e) => {onFileUpload(e);}}
       />
-      
+
       <pre>{JSON.stringify(excelData[0], null, 2)}</pre>
-      <button className="cursor-pointer flex items-center justify-center w-55 m-6 sm:w-64 sm:text-3sivoxl  h-12 sm:h-16 px-4 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 focus:bg-blue-600 text-xl" onClick={()=>{ forceRender();enviarUsuarios(); }} >Enviar</button>
+      <button className="cursor-pointer flex items-center justify-center w-55 m-6 sm:w-64 sm:text-2xl  h-12 sm:h-16 px-4 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 focus:bg-blue-600 text-xl" onClick={()=>{ forceRender();enviarUsuarios(); }} >Enviar</button>
     </div>
     
   );
