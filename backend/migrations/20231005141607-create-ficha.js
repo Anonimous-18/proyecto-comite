9d93c31ae13d5cbcc4f8cf4aa6fa7a2e71a33f9b
brayan <brayan6omez@gmail.com>
2023-10-05 10:38:46 -0500

    creacion de ficha

--- backend/migrations/20231005141607-create-ficha.js
+++ backend/migrations/20231005141607-create-ficha.js
@@ -2,21 +2,39 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('programas', {
+    await queryInterface.createTable('fichas', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      pro_codigo: {
+      fic_codigo: {
         type: Sequelize.STRING
       },
-      pro_nombre: {
+      fic_inicioLectiva: {
+        type: Sequelize.DATE
+      },
+      fic_finLectiva: {
+        type: Sequelize.DATE
+      },
+      fic_inicioProductiva: {
+        type: Sequelize.DATE
+      },
+      fic_finProductiva: {
+        type: Sequelize.DATE
+      },
+      fic_modalidad: {
         type: Sequelize.STRING
       },
-      pro_nivelFormacion: {
+      fic_jornada: {
         type: Sequelize.STRING
+      },
+      programa: {
+        type: Sequelize.STRING
+      },
+      instructor_id: {
+        type: Sequelize.INTEGER
       },
       createdAt: {
         allowNull: false,
@@ -29,6 +47,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('programas');
+    await queryInterface.dropTable('fichas');
   }
 };
