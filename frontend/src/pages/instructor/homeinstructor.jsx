fa2acbc75c687cc56f2d1d381201929cc45dd333
19SERGIO <sergiolondono55@gmail.com>
2023-10-09 08:33:21 -0500

    espacios

--- frontend/src/pages/instructor/homeinstructor.jsx
+++ frontend/src/pages/instructor/homeinstructor.jsx
@@ -113,7 +113,7 @@ export const Homeinstructor = () => {
   return (
     <DefaultLayout>
       <div>
-        <div className="mx-auto max-w-screen-xl pt-20 pb-32 sm:pt-20 sm:pb-40 ">
+        <div className="mx-auto max-w-screen-xl  ">
           <div className="h-auto max-w-full flex flex-col items-center  p-5 place-content-evenly rounded-2xl">
             <div className=" flex space-x-2">
               {/* flex space-x-4 para columas */}
