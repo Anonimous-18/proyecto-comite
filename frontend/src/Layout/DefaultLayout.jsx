888dee14d4c820c3d70616ce3bda5314e64ca1e6
brayan <brayan6omez@gmail.com>
2023-10-09 10:58:55 -0500

    perras responsive

--- frontend/src/Layout/DefaultLayout.jsx
+++ frontend/src/Layout/DefaultLayout.jsx
@@ -1,11 +1,13 @@
 import { NavBar } from "./NavBar";
 import { Footer } from "./Footer";
-export default function DefaultLayout ({ children }) {
-  return(
-    <div className="flex flex-col space-y-40 justify-between h-full">
-    <NavBar />
-    <div className=" items-center justify-center flex flex-col">{children}</div>
-    <Footer />
-  </div>
+export default function DefaultLayout({ children }) {
+  return (
+    <div className="flex flex-col space-y-40 justify-between h-full ">
+      <NavBar />
+      <div className="flex items-center justify-center border border-gray-950 w-full">
+        {children}
+      </div>
+      <Footer />
+    </div>
   );
 }
--- frontend/src/pages/admin/Form/FormularioRoles.jsx
+++ frontend/src/pages/admin/Form/FormularioRoles.jsx
@@ -1,5 +1,4 @@
-import { Footer } from "../../../Layout/Footer";
-import { NavBar } from "../../../Layout/NavBar";
+import DefaultLayout from "../../../Layout/DefaultLayout";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useContextApp } from "../../../Context/ContextApp";
@@ -52,27 +51,40 @@ export const FormularioRoles = () => {
   };
 
   return (
-    <>
-      <NavBar />
-
-      <main className="mt-28 h-full w-full flex flex-col">
-        <div className="mx-auto max-w-screen-xl pt-20 pb-32 sm:pt-48 sm:pb-40 ">
-          <div className="h-auto max-w-full flex flex-col items-center  p-5 place-content-evenly rounded-2xl">
-            <h1 className="mt-2  p-2 text-lg font-bold  text-blue-800  w-36 h-7 flex flex-col items-center rounded-l-lg pb-8">{params ? <>{params} Rol</> : <>Crear Rol</>}</h1>
-            <form
-              className="bg-gray-300 border-2 flex flex-col items-center p-2 rounded-lg "
-              onSubmit={(e) => handleSubmit(e)}
-            >
-              <label>Nombre: </label>
-              <input name="nombre" onChange={(e) => onChange(e)} type="text" className="rounded-lg p-2"/>
-              <button className=" mt-2 place-items-center flex flex-col items-center justify-center p-2 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-600 to-blue-800 group-hover:from-blue-600 group-hover:to-blue-800 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-sky-500 dark:focus:ring-blue-800" type="submit">
-                {params ? <>{params}</> : <>Crear</>}
-              </button>
-            </form>
+    <DefaultLayout>
+      <div className="h-full w-full md:w-1/2">
+        <form>
+          <div className="shadow-lg shadow-zinc-400  p-4 rounded-xl text-sm font-medium text-gray-900">
+            <h2 className="mb-4 text-xl font-bold text-blue-800 flex flex-col items-center">
+              Crear Rol
+            </h2>
+            <div>
+              <div>
+                <label
+                  for="first_name"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Nombre rol
+                </label>
+                <input
+                  type="text"
+                  id="first_name"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="John"
+                  required
+                />
+              </div>
+            </div>
+            <div className="p-2 sm:col-span-2 flex flex-row place-content-center">
+              <div>
+                <button className="right-0 ml-3 relative inline-flex items-center rounded-md border border-transparent bg-blue-700 px-10 py-2 text-xs font-bold text-white shadow-xl transition duration-300 ease-in-out hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-offset-2">
+                  Crear Solicitud
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-      </main>
-      <Footer />
-    </>
+        </form>
+      </div>
+    </DefaultLayout>
   );
 };
