38859c4f4224f617dab9dfecbef81cfbd07e60ad
19SERGIO <sergiolondono55@gmail.com>
2023-10-11 19:54:40 -0500

    diseño tabla

--- frontend/src/pages/instructor/TablaAntencedentesInstructor.jsx
+++ frontend/src/pages/instructor/TablaAntencedentesInstructor.jsx
@@ -48,21 +48,22 @@ export const TablaAntencedentesInstructor = () => {
   return (
     <DefaultLayout>
       <div className="mx-auto max-w-screen-xl">
-        <div className="h-auto max-w-full flex flex-col items-center  p-5 place-content-evenly rounded-2xl">
+        <div className="h-auto max-w-full flex flex-col items-center  p-5 place-content-evenly rounded-2xl ">
           <div>
-            <p className="">Filtrar por:</p>
-            <div>
-              <form onSubmit={(e) => handleSubmit(e)} className=" space-x-2 flex flex-row p-2">
-                <div>
+            <p className="flex flex-col items-center p-2 text-blue-800 text-2xl">Filtrar por:</p>
+            <div className=" text-base p-2">
+              <form onSubmit={(e) => handleSubmit(e)} className=" space-x-2 flex flex-row p-2 border rounded-md ">
+                <div className="">
                   <input
                     onChange={(e) => onChange(e)}
+                    className="border flex rounded-md"
                     type="number"
                     name="documento"
                     placeholder="Identificación"
                   />
                 </div>
                 <div>
-                  <button type="submit">
+                  <button type="submit" className=" text-3xl flex hover:text-blue-700 ">
                     <IoSearchCircle />
                   </button>
                 </div>
@@ -70,25 +71,25 @@ export const TablaAntencedentesInstructor = () => {
             </div>
           </div>
           {aprendices && aprendices !== null ? (
-            <table>
-              <tbody>
-                <tr>
-                  <th>Identificación</th>
-                  <th>Nombre Completo</th>
-                  <th>Email</th>
-                  <th>Telefono</th>
-                  <th>Acción</th>
+            <table className="w-full place-content-center ">
+              <tbody className="w-full md:w-4/6 shadow-lg shadow-zinc-400  p-4 justify-center text-gray-900 border-3 rounded-md items-center ">
+                <tr className=" justify-center ">
+                  <th className="border-2 justify-center items-center">Identificación</th>
+                  <th className="border-2 justify-center items-center">Nombre Completo</th>
+                  <th className="border-2 justify-center items-center">Email</th>
+                  <th className="border-2 justify-center items-center">Telefono</th>
+                  <th className="border-2 justify-center items-center">Acción</th>
                 </tr>
                 {aprendices && filtrado === null ? (
                   aprendices.map((aprendiz) => (
                     <tr key={aprendiz.id}>
-                      <td>{aprendiz.documento}</td>
-                      <td>{aprendiz.nombre_completo}</td>
-                      <td>{aprendiz.email}</td>
-                      <td>{aprendiz.telefono}</td>
-                      <td>
+                      <td className="border-2 justify-center items-center">{aprendiz.documento}</td>
+                      <td className="border-2 justify-center items-center">{aprendiz.nombre_completo}</td>
+                      <td className="border-2 justify-center items-center">{aprendiz.email}</td>
+                      <td className="border-2 justify-center items-center">{aprendiz.telefono}</td>
+                      <td className="border-2 justify-center items-center">
                         <Link
-                          className="rounded-md bg-teal-500"
+                          className="rounded-md bg-teal-500 items-center flex justify-center"
                           to={`/Antecedenteaprendiz/${aprendiz.id}`}
                         >
                           Ver
@@ -98,11 +99,11 @@ export const TablaAntencedentesInstructor = () => {
                   ))
                 ) : (
                   <tr key={filtrado[0].id}>
-                    <td>{filtrado[0].documento}</td>
-                    <td>{filtrado[0].nombre_completo}</td>
-                    <td>{filtrado[0].email}</td>
-                    <td>{filtrado[0].telefono}</td>
-                    <td>
+                    <td className="border-2 justify-center items-center">{filtrado[0].documento}</td>
+                    <td className="border-2 justify-center items-center">{filtrado[0].nombre_completo}</td>
+                    <td className="border-2 justify-center items-center">{filtrado[0].email}</td>
+                    <td className="border-2 justify-center items-center">{filtrado[0].telefono}</td>
+                    <td className="border-2 justify-center items-center">
                       <Link
                         className="rounded-md bg-teal-500"
                         to={`/Antecedenteaprendiz/${filtrado[0].id}`}
