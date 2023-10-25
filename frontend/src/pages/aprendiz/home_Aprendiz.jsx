19f29dd0e0cc01f40873b6dde6d1121e39cba55f
19SERGIO <sergiolondono55@gmail.com>
2023-10-25 07:41:26 -0500

    iconos y colores home

--- frontend/src/pages/aprendiz/home_Aprendiz.jsx
+++ frontend/src/pages/aprendiz/home_Aprendiz.jsx
@@ -2,7 +2,7 @@ import DefaultLayout from "../../Layout/DefaultLayout";
 import {TbAlignBoxRightTop} from "react-icons/tb";
 import {TiGroup} from "react-icons/ti";
 import {VscHistory} from "react-icons/vsc"
-import {GrDocumentUpload} from "react-icons/gr"
+import {IoDocumentOutline} from "react-icons/io5"
 import {HiDocumentDuplicate} from "react-icons/hi"
 import {MdNotificationsActive} from "react-icons/md"
 import { Link } from "react-router-dom";
@@ -16,9 +16,9 @@ export const Home_Aprendiz = () => {
             <div className="bg-white   py-10 flex flex-row justify-center items-center">
               <section className="gap-4">
                 <Link to="/pruebaaprendiz">
-                  <button className=" w-52  h-52 shadow-2xl border-gray-200 duration-150 border text-black hover:text-gray-200 bg-gray-400 hover:bg-gray-800 flex flex-col items-center justify-center rounded-lg">
+                  <button className=" w-52  h-52 shadow-2xl border-gray-400 duration-150 border text-black hover:text-gray-200 bg-gray-400 hover:bg-gray-600 flex flex-col items-center justify-center rounded-lg">
                     <TiGroup className=" text-9xl " />
-                    <div className=" bg-blue-600 rounded-b-lg w-full h-20 items-center justify-center hover:bg-blue-800 ">
+                    <div className=" bg-blue-500 rounded-b-lg w-full h-20 items-center justify-center hover:bg-blue-600 ">
                       <p className="mt-2 text-xl">Ver solicitudes a comite</p>
                     </div>
                      
@@ -58,7 +58,7 @@ export const Home_Aprendiz = () => {
               <section className="gap-4">
                 <Link to="/pruebaaprendiz">
                 <button className=" w-52  h-52 shadow-2xl border-gray-200 duration-150 border text-black hover:text-gray-200 bg-gray-400 hover:bg-gray-800 flex flex-col items-center justify-center rounded-lg">
-                    <GrDocumentUpload className=" text-9xl " />
+                    <IoDocumentOutline className=" text-9xl " />
                     <div className=" bg-blue-600 rounded-b-lg w-full h-20 items-center justify-center hover:bg-blue-800 ">
                       <p className="mt-2 text-xl">Subir pruebas</p>
                     </div>
