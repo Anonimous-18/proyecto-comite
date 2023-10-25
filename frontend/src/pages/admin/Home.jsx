61804c881a7db8c2c1e8da9d4c618ab42d9ddbb1
BrayanGN <brayan6omez@gmail.com>
2023-10-24 21:13:41 -0500

    c r e t y

--- frontend/src/pages/admin/Home.jsx
+++ frontend/src/pages/admin/Home.jsx
@@ -26,9 +26,9 @@ export const Home = () => {
 
   return (
     <div className="h-screen flex justify-center items-center">
-      <div className="flex justify-center items-center h-4/5 w-2/3 text-8xl text-white animate-pulse transform rotate-3 py-4 px-6 rounded-lg bg-gradient-to-r from-purple-400 to-pink-600 shadow-xl border-4 border-double border-yellow-500 tracking-wider hover:text-gray-200 hover:scale-105 hover:rotate-6 transition-all duration-300 mix-blend-overlay hover:bg-gradient-to-r hover:from-pink-500 hover:to-purple-600 hover:shadow-2xl 3d rotate-x-12 rotate-y-12 rotate-z-12">
+      <div className="flex justify-center items-center h-4/5 w-2/3 text-8xl text-white animate-pulse transform rotate-3 py-4 px-6 rounded-lg bg-gradient-to-r from-blue-400 to-blue-600 shadow-xl border-4 border-double tracking-wider hover:text-gray-200 hover:scale-105 hover:rotate-6 transition-all duration-300 mix-blend-overlay hover:bg-gradient-to-r hover:from-pink-500 hover:to-purple-600 hover:shadow-2xl 3d rotate-x-12 rotate-y-12 rotate-z-12">
         {" "}
-        <span class="animate-bounce">Ojete</span>
+        <span class="animate-bounce">Cargando</span>
       </div>
     </div>
   );
