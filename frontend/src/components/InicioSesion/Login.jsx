35311d47075ca4f897127c3eb633e0b71b8364eb
BrayanGN <brayan6omez@gmail.com>
2023-10-29 15:43:19 -0500

    normalize

--- frontend/src/components/InicioSesion/Login.jsx
+++ frontend/src/components/InicioSesion/Login.jsx
@@ -118,7 +118,7 @@ export const Login = () => {
                         name="documento"
                         onChange={(e) => onChange(e)}
                         id="documento"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                        className=" bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                         required
                       />
                     </div>
