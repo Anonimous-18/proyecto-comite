a5106d583e389a1e175d2956485a47b4d05fefbb
Rjchi <richardospina18@gmail.com>
2023-11-12 16:37:51 -0500

    [Style] Modificar los estilos del logueo

--- frontend/src/components/InicioSesion/Login.jsx
+++ frontend/src/components/InicioSesion/Login.jsx
@@ -3,7 +3,6 @@ import { ErrorMessage, Field, Formik } from "formik";
 
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { BiErrorAlt } from "react-icons/bi";
 
 import { useContextApp } from "../../Context/ContextApp";
 
@@ -23,123 +22,215 @@ export const Login = () => {
       }, 3000);
       console.log(err);
     }
-  }
+  };
 
   return (
     <>
-      <section className="dalinComoFondo dark:bg-gray-900">
-        {err ? (
-          <div className="bg-opacity-95 absolute flex flex-col items-center justify-center px-6 py-8 mx-auto h-full w-full lg:py-0">
-            <div className="text-ls font-bold w-96 h-60 flex flex-col justify-center items-center text-center">
-              <div
-                id="alert-additional-content-2"
-                className="px-4 mb-4 text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
-                role="alert"
+      <div className="h-screen md:flex">
+        <div className="relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center hidden">
+          <div>
+            <h1 className="text-white font-bold text-4xl font-sans">
+              SE-JustApp
+            </h1>
+            <p className="text-white mt-1">
+              Sistema de Gestión de Comites SENA
+            </p>
+            {/* <button
+              type="button"
+              className="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2"
+            >
+              Read More
+            </button> */}
+            <div className="flex mt-8 space-x-6 text-gray-600">
+              <Link
+                className="hover:opacity-75"
+                to="#"
+                target="_blank"
+                rel="noreferrer"
               >
-                <BiErrorAlt className=" w-64 h-36 md:h-auto  rounded-xl mx-auto border-2 " />
-                <div className="mt-2 mb-4 text-sm">
-                  Correo o contraseña incorrectas intente nuevamente
-                </div>
-              </div>
-            </div>
-          </div>
-        ) : (
-          <div className="flex items-center justify-center mx-auto h-screen">
-            <div className="w-auto rounded-lg  dark:border md:mt-0 sm:max-w-md xl:p-0 shadow-xl bg-blue-800">
-              <div className="p-1 space-y-4 md:space-y-6 sm:p-1">
-                <h3 className="text-center mt-4 mb-4 text-sm font-extrabold text-white lg:text-5xl md:text-4xl">
-                  {" "}
-                  Bienvenido a{" "}
-                  <span className="text-transparent bg-clip-text bg-gradient-to-r to-sky-500 from-sky-400">
-                    SE-JustApp
-                  </span>
-                </h3>
-                <Formik
-                  initialValues={{
-                    documento: "",
-                    contrasenia: "",
-                  }}
-                  validationSchema={Yup.object({
-                    documento: Yup.number()
-                      .min(1, "Minimo 15 caracteres")
-                      .required("El documento es requerido"),
-                    contrasenia: Yup.string()
-                      .min(1, "Minimo 8 caracteres")
-                      .required("La contraseña es requerida"),
-                  })}
-                  onSubmit={async (values, { resetForm }) => {
-                    await handleSubmit(values);
-                    resetForm();
-                  }}
-                  className="space-y-4 md:space-y-6"
+                <span className="sr-only"> Facebook </span>
+                <svg
+                  className="w-6 h-6"
+                  fill="black"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
                 >
-                  {(formik) => (
-                    <form onSubmit={formik.handleSubmit}>
-                      <div className="bg-white w-full h-full sm:p-7 rounded-lg p-4">
-                        <div>
-                          <label className="block mb-2 text-sm font-medium text-gray-900 ">
-                            Documento
-                          </label>
-                          <Field
-                            type="number"
-                            autoFocus
-                            name="documento"
-                            id="documento"
-                            className=" bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                          />
-                          <div className="text-red-600 font-bold">
-                            <ErrorMessage name="documento" />
-                          </div>
-                        </div>
-                        <div>
-                          <label className="block mb-2 text-sm font-medium text-gray-900 ">
-                            Contraseña:
-                          </label>
-                          <Field
-                            type="password"
-                            name="contrasenia"
-                            id="password"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                          />
-                          <div className="text-red-600 font-bold">
-                            <ErrorMessage name="contrasenia" />
-                          </div>
-                        </div>
-                        <div className="flex items-center justify-between">
-                          <Link
-                            to={`/recuperacion-contraseña`}
-                            className=" text-blue-800 text-sm font-medium text-primary-600 hover:underline pt-3 pb-4"
-                          >
-                            ¿Olvidaste tu contraseña?
-                          </Link>
-                        </div>
-                        <button
-                          // disabled={formik.isSubmitting || !formik.isValid}
-                          type="submit"
-                          className=" place-items-center flex flex-col items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-600 to-blue-800 group-hover:from-blue-600 group-hover:to-blue-800 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-sky-500 dark:focus:ring-blue-800"
-                        >
-                          <span className="relative px-11 py-2.5 transition-all ease-in duration-75 bg-blue-500 text-white rounded-md group-hover:bg-opacity-0">
-                            Inicio de Sesion
-                          </span>
-                        </button>
-                        <p className="text-sm font-light text-gray-700  pt-1 pb-2">
-                          Tienes cuenta?{" "}
-                          <Link
-                            to={`/register`}
-                            className="font-medium text-primary-600 hover:underline text-blue-800"
-                          >
-                            Registrarse
-                          </Link>
-                        </p>
-                      </div>
-                    </form>
-                  )}
-                </Formik>
-              </div>
+                  <path
+                    fillRule="evenodd"
+                    d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </Link>
+              <Link
+                className="hover:opacity-75"
+                to="#"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span className="sr-only"> Instagram </span>
+                <svg
+                  className="w-6 h-6"
+                  fill="black"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </Link>
+              <Link
+                className="hover:opacity-75"
+                to="#"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span className="sr-only"> Twitter </span>
+                <svg
+                  className="w-6 h-6"
+                  fill="black"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                </svg>
+              </Link>
+              <Link
+                className="hover:opacity-75"
+                to="#"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span className="sr-only"> GitHub </span>
+                <svg
+                  className="w-6 h-6"
+                  fill="black"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </Link>
             </div>
           </div>
-        )}
-      </section>
+          <div className="absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+          <div className="absolute -bottom-40 -left-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+          <div className="absolute -top-40 -right-0 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+          <div className="absolute -top-20 -right-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+        </div>
+        <div className="flex md:w-1/2 justify-center py-10 items-center bg-white">
+          <Formik
+            initialValues={{
+              documento: "",
+              contrasenia: "",
+            }}
+            validationSchema={Yup.object({
+              documento: Yup.number()
+                .min(1, "Minimo 15 caracteres")
+                .required("El documento es requerido"),
+              contrasenia: Yup.string()
+                .min(1, "Minimo 8 caracteres")
+                .required("La contraseña es requerida"),
+            })}
+            onSubmit={async (values, { resetForm }) => {
+              await handleSubmit(values);
+              resetForm();
+            }}
+            className="space-y-4 md:space-y-6"
+          >
+            {(formik) => (
+              <form onSubmit={formik.handleSubmit} className="bg-white">
+                <h1 className="text-gray-800 font-bold text-2xl mb-1">
+                  ¡Hola de Nuevo!
+                </h1>
+                <p className="text-sm font-normal text-gray-600 mb-7">
+                  No tienes cuenta?{" "}
+                  <span className="inline-block">
+                    <Link
+                      to={`/register`}
+                      className="text-sm ml-2 text-blue-500 hover:text-blue-700"
+                    >
+                      Registrarse
+                    </Link>
+                  </span>
+                </p>
+                <div className="text-red-600 font-bold text-xs text-center">
+                  {err ? (
+                    <p>Identificación o contraseña incorrecta(s)</p>
+                  ) : (
+                    <ErrorMessage name="documento" />
+                  )}
+                </div>
+                <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 text-gray-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <Field
+                    className="pl-2 outline-none border-none"
+                    type="number"
+                    autoFocus
+                    name="documento"
+                    placeholder="Identificación"
+                  />
+                </div>
+                <div className="text-red-600 font-bold text-xs text-center">
+                  <ErrorMessage name="contrasenia" />
+                </div>
+                <div className="flex items-center border-2 py-2 px-3 rounded-2xl">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 text-gray-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <Field
+                    className="pl-2 outline-none border-none"
+                    type="password"
+                    name="contrasenia"
+                    placeholder="Contraseña"
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  disabled={formik.isSubmitting}
+                  className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2"
+                >
+                  Iniciar Sesión
+                </button>
+                <Link
+                  to={`/recuperacion-contraseña`}
+                  className="text-sm ml-2 text-blue-500 hover:text-blue-700"
+                >
+                  Olvidaste tu Contraseña?
+                </Link>
+              </form>
+            )}
+          </Formik>
+        </div>
+      </div>
     </>
   );
 };
