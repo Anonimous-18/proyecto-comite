619572e3d6f0d8547860f6f20458ef33abebedfb
Rjchi <richardospina18@gmail.com>
2023-11-15 15:23:52 -0500

    [Style] Modificar estilos del formulario de solicitud de citación a comite

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -12,7 +12,7 @@ import Prueba from "./components/pruebas/Prueba";
 import { Spinner } from "./components/util/Spinner";
 import { useContextApp } from "./Context/ContextApp";
 import { Login } from "./components/InicioSesion/Login";
-import { Example } from "./components/tailwind/BlogList";
+import { Example } from "./components/tailwind/Form";
 import { Register1 } from "./components/InicioSesion/Register1";
 import { Reglamento } from "./components/Reglamento/Reglamento";
 import Recuperacion from "./components/InicioSesion/Recuperacion";
@@ -145,7 +145,7 @@ const Router = () => {
           {/* Rutas de Instructor */}
           <Route path="/home-instructor" element={<Homeinstructor />} />
           <Route path="/voto-comite/:comite" element={<Votoinstructor />} />
-          <Route path="/solicitudinstructor" element={<SolicitudIntructor />} />
+          <Route path="/solicitud-instructor" element={<SolicitudIntructor />} />
           <Route path="/novedades-instructor" element={<NovedadInstructor />} />
           <Route
             path="/antecedentes"
@@ -169,7 +169,7 @@ const Router = () => {
           {/* Rutas de Gestor */}
           <Route path="/home-gestor" element={<HomeGestor />} />
 
-          <Route path="*" element={<Navigate to={`${ruta}`} replace />} />  
+          <Route path="*" element={<Navigate to={`${ruta}`} replace />} />
         </Routes>
       ) : (
         <>
