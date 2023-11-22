618e50effba88e0fbfa5de4b4bdc8c18def09b4e
Rjchi <richardospina18@gmail.com>
2023-11-22 10:48:46 -0500

    [Refactor] Modifcar componente de ficha

--- frontend/src/components/util/Ficha.jsx
+++ frontend/src/components/util/Ficha.jsx
@@ -1,58 +1,61 @@
+import { CgDetailsMore } from "react-icons/cg";
+import { HiIdentification } from "react-icons/hi";
+import { Link } from "react-router-dom";
+
 export const Ficha = ({ ficha }) => {
   return (
     <li>
-      {/* <a href={application.href} className="block hover:bg-gray-50">
+      <div className="block hover:bg-gray-50">
         <div className="flex items-center px-4 py-4 sm:px-6">
           <div className="flex min-w-0 flex-1 items-center">
             <div className="flex-shrink-0">
               <img
                 className="h-12 w-12 rounded-full"
-                src={application.applicant.imageUrl}
+                src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                 alt=""
               />
             </div>
             <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
               <div>
                 <p className="truncate text-sm font-medium text-indigo-600">
-                  {application.applicant.name}
+                  {ficha.programa}
                 </p>
                 <p className="mt-2 flex items-center text-sm text-gray-500">
-                  {/* <EnvelopeIcon
-                          className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                          aria-hidden="true"
-                        /> */}
-                  {/* <span className="truncate">
-                    {application.applicant.email}
-                  </span>
+                  <span className="truncate">{"Codigo " + ficha.codigo}</span>
                 </p>
               </div>
               <div className="hidden md:block">
                 <div>
                   <p className="text-sm text-gray-900">
-                    Applied on{" "}
-                    <time dateTime={application.date}>
-                      {application.dateFull}
-                    </time>
+                    {ficha.inicioLectiva.replace(/T.*/, "")}
                   </p>
-                  <p className="mt-2 flex items-center text-sm text-gray-500"> */}
-                    {/* <CheckCircleIcon
-                            className="mr-1.5 h-5 w-5 flex-shrink-0 text-green-400"
-                            aria-hidden="true"
-                          /> */}
-                    {/* {application.stage}
+                  <p className="mt-2 flex items-center text-sm text-gray-500">
+                    <HiIdentification
+                      className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                      aria-hidden="true"
+                    />
+                    <span className="inline-block">
+                      Identificación + filtrado[0].documento
+                    </span>
                   </p>
                 </div>
               </div>
             </div>
           </div>
-          <div> */}
-            {/* <ChevronRightIcon
-                    className="h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                  /> */}
-          {/* </div>
+          <div>
+            <Link
+              className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              // to={`/antecedente-aprendiz/${filtrado[0].id}`}
+            >
+              Detalles
+              <CgDetailsMore
+                className="ml-3 -mr-1 h-5 w-5"
+                aria-hidden="true"
+              />
+            </Link>
+          </div>
         </div>
-      </a> */} 
+      </div>
     </li>
   );
 };
--- frontend/src/components/util/FichaList.jsx
+++ frontend/src/components/util/FichaList.jsx
@@ -1,46 +1,8 @@
 import { Ficha } from "./Ficha";
+import { Spinner } from "../util/Spinner";
 import { useEffect, useState } from "react";
 import DefaultLayout from "../../Layout/DefaultLayout";
-import { useContextApp } from "../../Context/ContextApp"; 
-
-const applications = [
-  {
-    applicant: {
-      name: "Ricardo Cooper",
-      email: "ricardo.cooper@example.com",
-      imageUrl:
-        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-    date: "2020-01-07",
-    dateFull: "January 7, 2020",
-    stage: "Completed phone screening",
-    href: "#",
-  },
-  {
-    applicant: {
-      name: "Kristen Ramos",
-      email: "kristen.ramos@example.com",
-      imageUrl:
-        "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-    date: "2020-01-07",
-    dateFull: "January 7, 2020",
-    stage: "Completed phone screening",
-    href: "#",
-  },
-  {
-    applicant: {
-      name: "Ted Fox",
-      email: "ted.fox@example.com",
-      imageUrl:
-        "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-    date: "2020-01-07",
-    dateFull: "January 7, 2020",
-    stage: "Completed phone screening",
-    href: "#",
-  },
-];
+import { useContextApp } from "../../Context/ContextApp";
 
 export const FichaList = () => {
   const [fichas, setFichas] = useState(null);
@@ -51,27 +13,21 @@ export const FichaList = () => {
       const res = await contextApi.getFichas();
 
       if (res) setFichas(res);
-    }
-    
+    };
+
     getFichas();
   }, [contextApi]);
-  console.log(fichas)
-  
+  console.log(fichas);
+
   return (
     <DefaultLayout>
       <div className="overflow-hidden bg-white shadow sm:rounded-md">
         <ul role="list" className="divide-y divide-gray-200">
-          {
-            fichas&&fichas.map((ficha) => {
-              <Ficha key={ficha.id} ficha/>
-            })
-          }
-          {applications.map((application) => (
-            <Ficha
-              key={application.applicant.email}
-              application={application}
-            />
-          ))}
+          {fichas ? (
+            fichas.map((ficha) => <Ficha key={ficha.id} ficha={ficha} />)
+          ) : (
+            <Spinner />
+          )}
         </ul>
       </div>
     </DefaultLayout>
