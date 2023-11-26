0c7f9d58d3c9e600a122ce541b4fa58a60d2bc42
BrayanGN <brayan6omez@gmail.com>
2023-11-25 20:10:22 -0500

    colocacion de opcion de CASO en el select

--- frontend/src/pages/gestorcomite/Formacta.jsx
+++ frontend/src/pages/gestorcomite/Formacta.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 
 export const FormActa = () => {
   const textAreaStyle = {
-    outline: "none",
+    all: "unset",
+    fontWeight: "bold"
   };
   const [divs, setDivs] = useState([]);
+  const [casoOption, setCasoOption] = useState(false);
 
   const handleClick = () => {
     setDivs([...divs, divs.length]);
@@ -13,6 +15,13 @@ export const FormActa = () => {
   const handleDelete = (index) => {
     setDivs(divs.filter((_, i) => i !== index));
   };
+  const handleSelect = (e)=>{
+    if(e.target.value === "CASO"){
+      setCasoOption(true);
+    }else{
+      setCasoOption(false);
+    }
+  }
   return (
     <div className="flex flex-col xl:flex-row min-h-screen bg-gray-100 ">
       <div className="m-auto">
@@ -99,7 +108,7 @@ export const FormActa = () => {
                   Contrato De Aprendizaje:
                 </p>
                 <input
-                  className="text-black placeholder-gray-600 px-4 py-2.5 mt-2 text-base transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400 m-auto w-[] "
+                  className="text-black placeholder-gray-600 px-4 py-2.5 mt-2 text-base transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400 m-auto ml-1 w-[60px] "
                   defaultValue={"No"}
                 />
                 <br />
@@ -110,7 +119,8 @@ export const FormActa = () => {
                   rows="2"
                   defaultValue={"Sin historial"}
                   style={textAreaStyle}
-                  className="text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"></textarea>
+                  className="text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"
+                ></textarea>
               </div>
               <div className="flex-none pt-2.5 pr-2.5 pl-1"></div>
             </div>
@@ -122,23 +132,23 @@ export const FormActa = () => {
               </div>
               <div className="flex-none pt-2.5 pr-2.5 pl-1"></div>
             </div>
-            <div className="flex justify-center">
-               
-            </div>
+            <div className="flex justify-center"></div>
 
             <hr className="mt-4" />
             <div className="flex flex-row-reverse p-3">
               <div className="flex-initial pl-3">
                 <button
                   type="button"
-                  className="flex items-center px-5 py-2.5 font-medium tracking-wide text-white capitalize bg-black rounded-md hover:bg-gray-800  focus:outline-none focus:bg-gray-900  transition duration-300 transform active:scale-95 ease-in-out">
+                  className="flex items-center px-5 py-2.5 font-medium tracking-wide text-white capitalize bg-black rounded-md hover:bg-gray-800  focus:outline-none focus:bg-gray-900  transition duration-300 transform active:scale-95 ease-in-out"
+                >
                   <span className="pl-2 mx-1">Save</span>
                 </button>
               </div>
               <div className="flex-initial">
                 <button
                   type="button"
-                  className="flex items-center px-5 py-2.5 font-medium tracking-wide text-black capitalize rounded-md  hover:bg-red-200 hover:fill-current hover:text-red-600  focus:outline-none  transition duration-300 transform active:scale-95 ease-in-out">
+                  className="flex items-center px-5 py-2.5 font-medium tracking-wide text-black capitalize rounded-md  hover:bg-red-200 hover:fill-current hover:text-red-600  focus:outline-none  transition duration-300 transform active:scale-95 ease-in-out"
+                >
                   <span className="pl-2 mx-1">Delete</span>
                 </button>
               </div>
@@ -169,27 +179,36 @@ export const FormActa = () => {
                   <div key={div} className="flex flex-col ">
                     <hr className="mt-4" />
                     <div className="flex flex-col sm:flex-row gap-2 m-5">
-                    <select className="p-3 w-60 sm:w-96">
-                      <option value="COORDINADORA ACADEMICA">
-                        COORDINADORA ACADEMICA
-                      </option>
-                      <option value="REPRESENTANTE DE BIENESTAR AL APRENDIZ">
-                        REPRESENTANTE DE BIENESTAR AL APRENDIZ
-                      </option>
-                      <option value="INSTRUCTOR">INSTRUCTOR</option>
-                      <option value="APOYO ADMINISTRATIVO A COORDINACIÓN ACADÉMICA DEL CENTRO">
-                        APOYO ADMINISTRATIVO A COORDINACIÓN ACADÉMICA DEL CENTRO
-                      </option>
-                      <option value="GESTOR FICHA">GESTOR FICHA</option>
-                      <option value="APRENDIZ">APRENDIZ</option>
-                    </select>
-                    <button
-                      onClick={() => handleDelete(index)}
-                      className="flex items-center px-5 py-2.5 font-medium tracking-wide text-white capitalize bg-black rounded-md hover:bg-gray-800  focus:outline-none focus:bg-gray-900  transition duration-300 transform active:scale-95 ease-in-out text-center max-w-[120px]">
-                      <span className="w-full text-center">Eliminar</span>
-                    </button>
+                      <select className="p-3 w-60 sm:w-96" onChange={handleSelect}>
+                        <option value="COORDINADORA ACADEMICA">
+                          COORDINADORA ACADEMICA
+                        </option>
+                        <option value="REPRESENTANTE DE BIENESTAR AL APRENDIZ">
+                          REPRESENTANTE DE BIENESTAR AL APRENDIZ
+                        </option>
+                        <option value="INSTRUCTOR">INSTRUCTOR</option>
+                        <option value="APOYO ADMINISTRATIVO A COORDINACIÓN ACADÉMICA DEL CENTRO">
+                          APOYO ADMINISTRATIVO A COORDINACIÓN ACADÉMICA DEL
+                          CENTRO
+                        </option>
+                        <option value="GESTOR FICHA">GESTOR FICHA</option>
+                        <option value="APRENDIZ">APRENDIZ</option>
+                        <option value="CASO">CASO</option>
+                      </select>
+                      <button
+                        onClick={() => handleDelete(index)}
+                        className="flex items-center px-5 py-2.5 font-medium tracking-wide text-white capitalize bg-black rounded-md hover:bg-gray-800  focus:outline-none focus:bg-gray-900  transition duration-300 transform active:scale-95 ease-in-out text-center max-w-[120px]"
+                      >
+                        <span className="w-full text-center">Eliminar</span>
+                      </button>
                     </div>
-                    <textarea name="" id="" rows="4" className="text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"></textarea>
+                    <input type="text" className="text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400" placeholder={casoOption?'Nombre aprendiz':"Nombre"} />
+                    <textarea
+                      name=""
+                      id=""
+                      rows="4"
+                      className="text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400" placeholder="Opinion o descripcion de caso"
+                    ></textarea>
                   </div>
                 ))}
               </div>
@@ -198,14 +217,16 @@ export const FormActa = () => {
                 <div className="flex-initial pl-3">
                   <button
                     type="button"
-                    className="flex items-center px-5 py-2.5 font-medium tracking-wide text-white capitalize bg-black rounded-md hover:bg-gray-800  focus:outline-none focus:bg-gray-900  transition duration-300 transform active:scale-95 ease-in-out">
+                    className="flex items-center px-5 py-2.5 font-medium tracking-wide text-white capitalize bg-black rounded-md hover:bg-gray-800  focus:outline-none focus:bg-gray-900  transition duration-300 transform active:scale-95 ease-in-out"
+                  >
                     <span className="pl-2 mx-1 ">Crear acta</span>
                   </button>
                 </div>
                 <div className="flex-initial">
                   <button
                     type="button"
-                    className="flex items-center px-5 py-2.5 font-medium tracking-wide text-black capitalize rounded-md  hover:bg-red-200 hover:fill-current hover:text-red-600  focus:outline-none  transition duration-300 transform active:scale-95 ease-in-out">
+                    className="flex items-center px-5 py-2.5 font-medium tracking-wide text-black capitalize rounded-md  hover:bg-red-200 hover:fill-current hover:text-red-600  focus:outline-none  transition duration-300 transform active:scale-95 ease-in-out"
+                  >
                     <span className="pl-2 mx-1">Delete</span>
                   </button>
                 </div>
